01f1db2fe25d4a656d4760cacbf05c71676a87a8
Daniel Naranjo <dnaranjo@gmail.com>
2018-04-17 14:24:34 -0300

    Initial

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->string('address')->after('password')->nullable();
+            $table->string('phone')->after('address')->nullable();
+            $table->string('avatar')->nullable();
+            $table->integer('dni')->nullable();
+    		$table->date('birthdate')->nullable();
+    		$table->enum('gender', ['NA', 'F', 'M'])->nullable();
+    		$table->string('city')->nullable();
+    		$table->string('province')->nullable();
+            $table->integer('country')->nullable();
+            $table->string('ipAddress')->nullable();
+            $table->string('provider')->nullable();
+     		$table->string('provider_id')->nullable();
+            $table->char('verified', 1)->default(0);
+            $table->char('status', 1)->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
