8d443fad0534d96d7882751f6160838f69f0fbeb
Daniel Naranjo <dnaranjo@gmail.com>
2018-04-17 16:04:07 -0300

    Migrations fixes

--- database/migrations/2018_04_17_170101_create_products_table.php
+++ database/migrations/2018_04_17_170101_create_products_table.php
@@ -17,10 +17,11 @@ class CreateProductsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->text('description');
+            $table->char('category', 1)->default(1);
             $table->float('price', 2);
             $table->float('sale', 2)->nullable();
             $table->float('cost', 2)->nullable();
-            $table->integer('stock', 11)->default(10);
+            $table->integer('stock')->default(10);
             $table->string('provider')->nullable();
             $table->string('url')->nullable();
             $table->string('dimensions')->nullable();
